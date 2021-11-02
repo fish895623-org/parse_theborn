953076885e2182c38f9a8a55fcc5cc678686a8ff
fish895623 <dan990429@gmail.com>
2021-11-02 16:54:55 +0900

    Save information as json

--- src/main.py
+++ src/main.py
@@ -10,29 +10,37 @@ options.add_argument("no-sandbox")
 options.add_argument("window-size=1920x1080")
 options.add_argument("lang=ko_KR")
 options.add_argument("log-level=3")
-# options.add_argument("headless")
+options.add_argument("headless")
 
 driver = webdriver.Chrome("chromedriver.exe", options=options)
 driver.get("https://www.theborn.co.kr/store/domestic-store/")
 
 select = Select(driver.find_element_by_xpath('//*[@id="select_brand"]'))
 select.select_by_value("홍콩반점0410")
+
+store_name = []
+store_address = []
+
 try:
     while True:
         sleep(1)
         driver.find_element_by_xpath("//i[@class='fa fa-angle-right']").click()
         sleep(1)
-        elements = driver.find_elements_by_css_selector(
+        store = driver.find_elements_by_css_selector(
             "#store_list > div > ul > li:nth-child(3)"
         )
         address = driver.find_elements_by_css_selector(
             "#store_list > div > ul > li:nth-child(4)"
         )
-        for element in elements:
-            print(element.text)
+        for element in store:
+            store_name.append(element.text)
+
+        for element in address:
+            store_address.append(element.text)
 
 except NoSuchElementException:
     print("finished")
 
+assert len(store_name) == len(store_address)
 
 driver.close()
