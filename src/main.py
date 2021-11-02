80f267b1ef46d1cf715109cd69942ef3a36fd992
fish895623 <34264960+fish895623@users.noreply.github.com>
2021-11-02 15:39:41 +0900

    parse store name (#2)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
@@ -9,13 +9,21 @@ options.add_argument("no-sandbox")
 options.add_argument("window-size=1920x1080")
 options.add_argument("lang=ko_KR")
 options.add_argument("log-level=3")
+options.add_argument("headless")
 
 driver = webdriver.Chrome("chromedriver.exe", options=options)
 driver.get("https://www.theborn.co.kr/store/domestic-store/")
 
 select = Select(driver.find_element_by_xpath('//*[@id="select_brand"]'))
 select.select_by_value("홍콩반점0410")
-time.sleep(1)
+sleep(1)
 driver.find_element_by_css_selector("#pagination > li:nth-child(6) > i").click()
+sleep(1)
+elements = driver.find_elements_by_css_selector(
+    "#store_list > div > ul > li:nth-child(3)"
+)
 
-driver.quit()
+for element in elements:
+    print(element.text)
+
+driver.close()
