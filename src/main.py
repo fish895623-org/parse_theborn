5ea1fa291a23fe3d87501de756e5b76dea4859d2
fish895623 <dan990429@gmail.com>
2021-11-02 16:20:25 +0900

    move to end of page using xpath

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 
 options = webdriver.ChromeOptions()
 
@@ -9,21 +10,29 @@ options.add_argument("no-sandbox")
 options.add_argument("window-size=1920x1080")
 options.add_argument("lang=ko_KR")
 options.add_argument("log-level=3")
-options.add_argument("headless")
+# options.add_argument("headless")
 
 driver = webdriver.Chrome("chromedriver.exe", options=options)
 driver.get("https://www.theborn.co.kr/store/domestic-store/")
 
 select = Select(driver.find_element_by_xpath('//*[@id="select_brand"]'))
 select.select_by_value("홍콩반점0410")
-sleep(1)
-driver.find_element_by_css_selector("#pagination > li:nth-child(6) > i").click()
-sleep(1)
-elements = driver.find_elements_by_css_selector(
-    "#store_list > div > ul > li:nth-child(3)"
-)
+try:
+    while True:
+        sleep(1)
+        driver.find_element_by_xpath("//i[@class='fa fa-angle-right']").click()
+        sleep(1)
+        elements = driver.find_elements_by_css_selector(
+            "#store_list > div > ul > li:nth-child(3)"
+        )
+        address = driver.find_elements_by_css_selector(
+            "#store_list > div > ul > li:nth-child(4)"
+        )
+        for element in elements:
+            print(element.text)
 
-for element in elements:
-    print(element.text)
+except NoSuchElementException:
+    print("finished")
+
 
 driver.close()
