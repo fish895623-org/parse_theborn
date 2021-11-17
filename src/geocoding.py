5321c777024775019a469cfefacbf3e46356c5d0
autoblack <fish895623@users.noreply.github.com>
2021-11-17 11:47:28 +0000

    fixup: Format Python code with Black

--- src/geocoding.py
+++ src/geocoding.py
@@ -5,7 +5,10 @@ import json
 # --data-urlencode "coordinate={검색_중심_좌표}" \
 # -H "X-NCP-APIGW-API-KEY-ID: {애플리케이션 등록 시 발급받은 client id값}" \
 # -H "X-NCP-APIGW-API-KEY: {애플리케이션 등록 시 발급받은 client secret값}" -v
-headers = {"X-NCP-APIGW-API-KEY-ID": "54sc1ydcbt", "X-NCP-APIGW-API-KEY": "ruiLGEgPms9vVlfNr5T6TR5ln5nyNAMkto6qvzlP"}
+headers = {
+    "X-NCP-APIGW-API-KEY-ID": "54sc1ydcbt",
+    "X-NCP-APIGW-API-KEY": "ruiLGEgPms9vVlfNr5T6TR5ln5nyNAMkto6qvzlP",
+}
 params = {"query": "경기 안산시 단원구 선부광장북로 36 상가동 210호(선부동, 벽산블루밍아파트)"}
 
 
@@ -15,13 +18,14 @@ params = {"query": "경기 안산시 단원구 선부광장북로 36 상가동 2
 #     params=params,
 # )
 
+
 def parse_json() -> dict:
-    with open("sample.json", "r", encoding='utf-8') as f:
+    with open("sample.json", "r", encoding="utf-8") as f:
         data = json.load(f)
     return data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     data = parse_json()
     for key in data:
         print(data[key])
